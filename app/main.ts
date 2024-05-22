011a25f4512e5bb559e0a302514d3ce36c59336f
Paulo Lacerda <paulo@findme.id>
2024-05-22 03:58:09 -0300

    pass 1st stage

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,6 @@ const server = net.createServer((socket) => {
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
-// server.listen(4221, 'localhost', () => {
-//     console.log('Server is running on port 4221');
-// });
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
+});
