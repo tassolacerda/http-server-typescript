b5509921bf462e7b333c691606d129e19e3fd05e
Paulo Lacerda <paulo@findme.id>
2024-05-23 05:40:45 -0300

    feat: add response get

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,22 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-    socket.end();
+    socket.on('data', (data) => {
+        const request = data.toString().trim();
+
+        if (request === '/') {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write('Unknown route');
+        }
+        socket.end();
+    })
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+
+
 
 server.listen(4221, 'localhost', () => {
     console.log('Server is running on port 4221');
