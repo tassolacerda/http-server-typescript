8a87ca9229c823aff77f663516d450c4e9ece8e1
Paulo Lacerda <paulo@findme.id>
2024-05-24 13:07:34 -0300

    feat: add new response get routes /

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,16 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
+    socket.on('data', (data) => {
+        const request = data.toString().trim();
+
+        if (request === '/') {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write('HTTP/1.1 404 Not Found\\r\\n\\r\\n');
+        }
+    })
     socket.end();
 });
 
